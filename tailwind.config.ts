f543055687ff7dd12b7b569eec91d17c36f9dab9
zerocrow-code <zerocrowofficial@gmail.com>
2026-02-18 22:57:45 -0600

    Create tailwind.config.ts for dark theme with purple accents

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          50: '#f5f3ff',
+          100: '#e5e3ff',
+          200: '#d5d3ff',
+          300: '#a8a3ff',
+          400: '#8b5bf3',
+          500: '#6d28d9',
+          600: '#5b21b6',
+          700: '#4c1d95',
+          800: '#3b0e6b',
+          900: '#1e1a4d'
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
